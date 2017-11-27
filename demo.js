5c90e1493548c1fb1b9e21cea5da5d9cfd60b0c3
zerolhao <zerolhao@gmail.com>
2017-11-27 20:07:35 +0800

    amend all

--- demo.js
+++ demo.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
 var fs = require('fs')
-
 var dirName = process.argv[2]
+if (fs.existsSync(dirName)) {
+  console.log('error: dir exists')
+  process.exit(1)
+} 
+else {
+
 
 fs.mkdirSync("./" + dirName)
 process.chdir("./" + dirName)
@@ -13,3 +18,6 @@ fs.mkdirSync("js")
 fs.writeFileSync("./index.html","<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
 fs.writeFileSync("css/style.css", "h1{color: red;}")
 fs.writeFileSync("./js/main.js", "var string = 'Hello World!'\nalert(string)")
+
+process.exit(0)
+}
--- demo.sh
+++ demo.sh
@@ -6,14 +6,11 @@ mkdir $1
 cd $1
 mkdir css js
 
-echo "<!DOCTYPE>
-<title>Hello</title>
-<h1>Hi</h1>" > index.html
+echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
 
-echo "h1{color: red;}" > css/style.css
+echo -e "h1{color: red;}" > css/style.css
 
-echo 'var string = "Hello World"
-alert(string)' > js/main.js
+echo -e 'var string = "Hello World"\nalert(string)' > js/main.js
 
 echo 'success'
 exit
