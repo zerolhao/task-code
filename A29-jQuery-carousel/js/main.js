9b62c1d67a3865eeea41e908c5975008fb029953
zerolhao <zerolhao@gmail.com>
2018-01-28 20:49:07 +0800

    add A29-jQuery-carousel

---
+++ A29-jQuery-carousel/js/main.js
@@ -0,0 +1,35 @@
+var $allBtns = $('#buttons > span') // 获取所有 button
+// 遍历所有btn，为每一个btn添加click事件
+for (let i = 0; i < $allBtns.length; i++) {
+  $allBtns.eq(i).on('click', function(e) {
+    let index = $(e.currentTarget).index() // 获取当前btn在它兄弟元素的下标
+    let px = index * -300 // 计算要移动的距离
+    $('#images').css({ // 为images添加 style
+      transform: 'translate(' + px + 'px)'
+    })
+    n = index
+    $allBtns.eq(index).addClass('redText')
+      .siblings('.redText').removeClass('redText')
+  })
+}
+// 添加自动轮播
+var n = 0
+var len = $allBtns.length
+$allBtns.eq(n % len).trigger('click')
+n++
+var timerId = setTimer()
+
+function setTimer() {
+  return setInterval(function() {
+    $allBtns.eq(n % len).trigger('click')
+    n++
+  }, 1000)
+}
+
+// 添加鼠标悬停
+$('.window').on('mouseover', function() {
+  clearInterval(timerId)
+})
+$('.window').on('mouseout', function() {
+  timerId = setTimer()
+})
