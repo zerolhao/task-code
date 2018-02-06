3743575c419bb6d4dead6014ca37445ca9533186
zerolhao <zerolhao@gmail.com>
2018-02-06 20:05:19 +0800

    update task-A30 main.js

--- A30-jQuery-seamless-carousel/main.js
+++ A30-jQuery-seamless-carousel/main.js
@@ -1,20 +1,36 @@
 var $imgs = $('.window > img')
-var n = 0
-$imgs.eq(n).addClass('active')
-$imgs.eq(n).siblings().addClass('ready')
-
-setInterval(()=>{
-  $imgs.eq(n).addClass('leave').removeClass('active')
-  .one('transitionend',(e)=>{
-    $(e.currentTarget).addClass('ready').removeClass('leave')
-  })
-  $imgs.eq(checkN(n+1)).addClass('active').removeClass('ready')
+var n
+init()
+setInterval(() => {
+  makeLeave($imgs.eq(n))
+    .one('transitionend', (e) => {
+      makeReady($(e.currentTarget))
+    })
+  makeActive($imgs.eq(checkN(n + 1)))
   n = checkN(++n)
-},1300)
-function checkN(n){
-  if(n === $imgs.length){
+}, 1300)
+
+
+
+function checkN(n) {
+  if (n === $imgs.length) {
     return 0
-  } else{
+  } else {
     return n
   }
+}
+
+function init() {
+  n = 0
+  $imgs.eq(n).addClass('active')
+  $imgs.eq(n).siblings().addClass('ready')
+}
+function makeLeave($obj){
+  return $obj.addClass('leave').removeClass('active')
+}
+function makeReady($obj){
+  return $obj.addClass('ready').removeClass('leave')
+}
+function makeActive($obj){
+  return $obj.addClass('active').removeClass('ready')
 }
