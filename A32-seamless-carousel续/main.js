61632cae7b9fe8416bc2ac502d8c054fcf45d169
zerolhao <zerolhao@gmail.com>
2018-03-02 22:16:28 +0800

    add 标签页被隐藏时停止播放

--- A32-seamless-carousel续/main.js
+++ A32-seamless-carousel续/main.js
@@ -15,31 +15,41 @@ $btns.on('click', function(e) { //  $('#buttonWrapper').on('click', 'button', fu
 })
 
 // 点击上一张或下一张切换
-$('#previous').on('click',function(){
-  goToSlide(current-1)
+$('#previous').on('click', function() {
+  goToSlide(current - 1)
 })
-$('#next').on('click',function(){
-  goToSlide(current+1)
+$('#next').on('click', function() {
+  goToSlide(current + 1)
 })
 
 // 自动切换
-let timer = setInterval(function(){
-  goToSlide(current+1)
-},2000)
+let timer = setInterval(function() {
+  goToSlide(current + 1)
+}, 2000)
 // 鼠标移入暂停，移出继续
-$('.container').on('mouseenter', function(){
+$('.container').on('mouseenter', function() {
   window.clearInterval(timer)
-}).on('mouseleave', function(){
-  timer = setInterval(function(){
-    goToSlide(current+1)
-  },2000)
+}).on('mouseleave', function() {
+  timer = setInterval(function() {
+    goToSlide(current + 1)
+  }, 2000)
+})
+// 当标签页被隐藏时停止播放
+document.addEventListener('visibilitychange', function(e) {
+  if (document.hidden) {
+    window.clearInterval(timer)
+  } else {
+    timer = setInterval(function() {
+      goToSlide(current + 1)
+    }, 2000)
+  }
 })
 
 function goToSlide(index) {
-  if(index > $btns.length -1){
+  if (index > $btns.length - 1) {
     index = 0
-  }else if(index < 0){
-    index = $btns.length -1
+  } else if (index < 0) {
+    index = $btns.length - 1
   }
   if (current === $btns.length - 1 && index === 0) { // 最后一张到第一张
     $slides.css({ transform: `translateX(${-($btns.length + 1) * 300}px)` })
@@ -57,7 +67,7 @@ function goToSlide(index) {
     $slides.css({ transform: `translateX(${-(index + 1) * 300}px)` })
   }
   current = index // 更新
-  console.log(current,index)
+  console.log(current, index)
 }
 
 function makeFakeSlides() {
