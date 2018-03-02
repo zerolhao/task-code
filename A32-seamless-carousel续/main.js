b915a9856e7a2f03c978f4efbcc7ac8cd439cf5d
zerolhao <zerolhao@gmail.com>
2018-03-02 22:09:56 +0800

    添加切换前后按钮、自动切换、鼠标悬停

--- A32-seamless-carousel续/main.js
+++ A32-seamless-carousel续/main.js
@@ -7,66 +7,58 @@ makeFakeSlides()
 
 $slides.css({ transform: 'translateX(-300px)' })
 
-$btns.on('click', function(e){
+// 点击图片对应按钮切换
+$btns.on('click', function(e) { //  $('#buttonWrapper').on('click', 'button', function(e){写法不同
   let $btn = $(e.currentTarget)
   let index = $btn.index()
-  if(current === $btns.length-1 && index === 0){ // 最后一张到第一张
+  goToSlide(index)
+})
+
+// 点击上一张或下一张切换
+$('#previous').on('click',function(){
+  goToSlide(current-1)
+})
+$('#next').on('click',function(){
+  goToSlide(current+1)
+})
+
+// 自动切换
+let timer = setInterval(function(){
+  goToSlide(current+1)
+},2000)
+// 鼠标移入暂停，移出继续
+$('.container').on('mouseenter', function(){
+  window.clearInterval(timer)
+}).on('mouseleave', function(){
+  timer = setInterval(function(){
+    goToSlide(current+1)
+  },2000)
+})
+
+function goToSlide(index) {
+  if(index > $btns.length -1){
+    index = 0
+  }else if(index < 0){
+    index = $btns.length -1
+  }
+  if (current === $btns.length - 1 && index === 0) { // 最后一张到第一张
     $slides.css({ transform: `translateX(${-($btns.length + 1) * 300}px)` })
-    .one('transitionend', function(){
-      $slides.hide().offset() // 只需要知道不加offset不行，原理不明
-      $slides.css({ transform: `translateX(${-(index + 1) * 300}px)` }).show()
-    })
-  } else if(current === 0 && index === $btns.length-1){ // 第一张到最后一张
+      .one('transitionend', function() {
+        $slides.hide().offset() // 只需要知道不加offset不行，原理不明
+        $slides.css({ transform: `translateX(${-(index + 1) * 300}px)` }).show()
+      })
+  } else if (current === 0 && index === $btns.length - 1) { // 第一张到最后一张
     $slides.css({ transform: 'translateX(0px)' })
-    .one('transitionend', function(){
-      $slides.hide().offset()
-      $slides.css({ transform: `translateX(${-(index + 1) * 300}px)` }).show()
-    })
-  } else{
+      .one('transitionend', function() {
+        $slides.hide().offset()
+        $slides.css({ transform: `translateX(${-(index + 1) * 300}px)` }).show()
+      })
+  } else {
     $slides.css({ transform: `translateX(${-(index + 1) * 300}px)` })
   }
   current = index // 更新
-})
-/*
-$btns.eq(0).on('click', function() {
-  if(current === 4){ // 最后一张到第一张
-    $slides.css({ transform: 'translateX(-1800px)' })
-    .one('transitionend', function(){
-      $slides.hide().offset() // 只需要知道不加offset不行，原理不明
-      $slides.css({ transform: 'translateX(-300px)' }).show()
-    })
-  } else{
-    $slides.css({ transform: 'translateX(-300px)' })
-  }
-  current = 0
-})
-$btns.eq(1).on('click', function() {
-  $slides.css({ transform: 'translateX(-600px)' })
-  current = 1
-})
-$btns.eq(2).on('click', function() {
-  $slides.css({ transform: 'translateX(-900px)' })
-  current = 2
-})
-$btns.eq(3).on('click', function() {
-  $slides.css({ transform: 'translateX(-1200px)' })
-  current = 3
-})
-$btns.eq(4).on('click', function() {
-  if(current === 0){ // 第一张到最后一张
-    $slides.css({ transform: 'translateX(0px)' })
-    .one('transitionend', function(){
-      $slides.hide().offset()
-      $slides.css({ transform: 'translateX(-1500px)' }).show()
-    })
-  } else{
-    $slides.css({ transform: 'translateX(-1500px)' })
-  }
-  current = 4
-})*/
-
-
-/*  工具函数   */
+  console.log(current,index)
+}
 
 function makeFakeSlides() {
   let $images = $slides.children('img')
