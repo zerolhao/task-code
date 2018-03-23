1888919abf87965619d07d34c5524a3cb4b2676f
zerolhao <zerolhao@gmail.com>
2018-03-24 00:49:59 +0800

    add sass loader

--- H45-webpack-demo/webpack.config.js
+++ H45-webpack-demo/webpack.config.js
@@ -8,14 +8,25 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
       }
-    }]
+    ]
   }
 };
